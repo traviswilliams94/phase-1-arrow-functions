fec02c8b2b224d78fb8e7f8bb54edb628bdbbb6a
Travis <travis.williams94@gmail.com>
2023-05-22 16:14:45 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const a = 2000
+const b  = 100
+
+function divide(a,b){
+    return a/b
+}
+
+divide(a,b);
+
+const square = parameter => parameter*parameter;
+
+const add = (parameter1, parameter2) => (parameter1 + parameter2);
